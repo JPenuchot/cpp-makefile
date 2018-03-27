b46021c48c1c39905a8571ea6bb5e2b29b3cacc4
Jules Pénuchot <jules.penuchot@lri.fr>
2018-03-27 14:01:20 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -4,40 +4,36 @@
 
 ORIGIN=$(pwd)
 
-DEFAULT_CMAKE_BUILD_DIR="build"
+DEFAULT_CMAKE_BUILD_DIR="bin"
 BUILD_SUBDIR_NAME=".build_files"
+
+EXEC_NAME="prog"
 
 CMAKE_BUILD_DIR=${1}
 OUTPUT_DIR=${2}
 
-if [ -z "${CMAKE_BUILD_DIR}" ]; then
-  CMAKE_BUILD_DIR=${DEFAULT_CMAKE_BUILD_DIR}
-fi
-
 ## Making directories
 
-mkdir -p ${CMAKE_BUILD_DIR}
-mkdir -p ${CMAKE_BUILD_DIR}/release
-mkdir -p ${CMAKE_BUILD_DIR}/debug
+mkdir -p ${ORIGIN}/${CMAKE_BUILD_DIR}/{release,debug}
 
 ## BUILD
 
 # Release
-cd ${CMAKE_BUILD_DIR}/release
+cd ${ORIGIN}/&{CMAKE_BUILD_DIR}/release
 mkdir -p ${BUILD_SUBDIR_NAME}
 cd ${BUILD_SUBDIR_NAME}
-cmake -DCMAKE_BUILD_TYPE=Release ../../..
+cmake -DCMAKE_BUILD_TYPE=Release ${ORIGIN}
 make
-mv prog ..
+mv ${EXEC_NAME} ..
 cd ..
-objdump -dC prog > prog.asm
+objdump -dC ${EXEC_NAME} > ${EXEC_NAME}.asm
 
 # Debug
-cd ../debug
+cd ${ORIGIN}/&{CMAKE_BUILD_DIR}/debug
 mkdir -p ${BUILD_SUBDIR_NAME}
 cd ${BUILD_SUBDIR_NAME}
-cmake -DCMAKE_BUILD_TYPE=Debug ../../..
+cmake -DCMAKE_BUILD_TYPE=Debug ${ORIGIN}
 make
-mv prog ..
-cd ../
-objdump -dC prog > prog.asm
+mv ${EXEC_NAME} ..
+cd ..
+objdump -dC ${EXEC_NAME} > ${EXEC_NAME}.asm
